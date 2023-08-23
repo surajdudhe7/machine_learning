a83fc17e088f325c65cefbd20f7900d2bbb5b493
Suraj Dudhe <119352724+surajdudhe7@users.noreply.github.com>
2023-08-24 00:33:14 +0530

    commit changes

--- model_2/callback_model.py
+++ model_2/callback_model.py
@@ -25,12 +25,10 @@ callbacks = myCallback()
 BATCH_SIZE = 32
 ds_train = ds_train.map(lambda x, y: (tf.cast(x, tf.float32)/255.0, y)).batch(BATCH_SIZE)
 ds_test = ds_test.map(lambda x, y: (tf.cast(x, tf.float32)/255.0, y)).batch(BATCH_SIZE)
-# Define the model
 
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
                                     tf.keras.layers.Dense(64, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
-# Compile data
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
